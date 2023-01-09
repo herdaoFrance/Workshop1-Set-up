cb6c296ab07bc53a24b5508a340968c70059c66f
herdaoFrance <122298552+herdaoFrance@users.noreply.github.com>
2023-01-09 19:08:16 +0100

    Create smartcontract.sol

---
+++ smartcontract.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.8.0;
+
+// Définition du contrat NFT
+contract NFT {
+    // Identifiant unique de chaque jeton
+    uint256 public tokenId;
+    
+    // Propriétaire du jeton
+    address public owner;
+    
+    // Constructeur du contrat qui assigne le propriétaire et l'identifiant unique au jeton
+    constructor(uint256 _tokenId, address _owner) public {
+        tokenId = _tokenId;
+        owner = _owner;
+    }
+    
+    // Fonction pour transférer la propriété d'un jeton à un autre compte
+    function transfer(address _to) public {
+        // Vérifier que l'appelant est le propriétaire actuel du jeton
+        require(msg.sender == owner, "Seul le propriétaire peut transférer un jeton");
+        
+        // Transférer la propriété du jeton à l'adresse spécifiée
+        owner = _to;
+    }
+}
